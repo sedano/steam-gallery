3de0ba5eeaceb9a3eab0ff1c59fc5e01f867add9
sedano <alejandro@sedano.me>
2016-12-03 18:36:37 +0100

    Add steam gallery

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var express = require('express')
 var app = express()
 
+var gallery = require('express-photo-gallery')
+
+var options = {
+  title: 'Sedano\'s Steam Gallery'
+}
+
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname));
+app.use('/', gallery('steam', options));
+
  
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
